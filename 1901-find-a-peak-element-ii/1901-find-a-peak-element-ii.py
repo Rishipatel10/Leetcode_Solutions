595d0fc0db0e64dede27c63af1fc017bc29dc8c5
Rishi Patel <rishipatel853@gmail.com>
2026-07-31 19:24:17 +0530

    Time: 31 ms (8.22%), Space: 46.2 MB (62.83%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.py
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.py
@@ -1,8 +1,8 @@
 class Solution:
     def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
-        maxi = float('-inf')
-        row = -1
-        col = -1
+        maxi = mat[0][0]
+        row = 0
+        col = 0
 
         for i in range(len(mat)):
             for j in range(len(mat[0])):
@@ -10,7 +10,7 @@ class Solution:
                     maxi = mat[i][j]
                     row = i
                     col = j
-                    
+                            
         for i in range(len(mat)):
             for j in range(len(mat[0])):
                 if maxi == mat[i][j]:
